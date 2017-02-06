197ddf78a3b87d5892e9c3f274640afb3280dc3f
Julia Kester <JKester@E2CCB.local>
2017-02-06 14:22:03 -0500

    Day 9 some work

--- Day9.js
+++ Day9.js
@@ -5,15 +5,26 @@ function parseInput(){
 	var pat = /^(\w+)\sto\s(\w+)\s.*\s(\d+)$/;
 
 	var distances = {};
+	var locations = [];
 
 	for(line of lines){
 		var match = line.match(pat);
-		var locations = [match[1], match[2]];
+		var currentLocs = [match[1], match[2]];
 
-		distances[match[3]] = locations;
+		distances[match[3]] = currentLocs;
+		if(!locations.includes(match[1])) locations.push(match[1]);
+		if(!locations.includes(match[2])) locations.push(match[2]);
 
 	}
+	console.log(locations);
 
-	console.dir(distances);
+	for(loc in locations){//for every location
+		navPaths(0, loc, locations);
+	}
 
+}
+
+function navPaths(totDistance, currentLoc, remaining){
+	//first remove currentLoc from list of remaining
+	remaining.pop(currentLoc);
 }

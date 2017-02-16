44b5bd258c47b5d5efc175f9599f39cfeaedb014
Julia Kester <JKester@E2CCB.local>
2017-02-16 10:42:15 -0500

    Day 9 problems

--- Day9.js
+++ Day9.js
@@ -1,10 +1,12 @@
+var distances = {};
+var shortest = 100000000;
+
 function parseInput(){
 	var text = document.getElementById("text").value;
 	var lines = text.split('\n');
 
 	var pat = /^(\w+)\sto\s(\w+)\s.*\s(\d+)$/;
 
-	var distances = {};
 	var locations = [];
 
 	for(line of lines){
@@ -16,15 +18,53 @@ function parseInput(){
 		if(!locations.includes(match[2])) locations.push(match[2]);
 
 	}
-	console.log(locations);
+
+	//console.log(distances);
+	//console.log("locations: " + locations);
 
 	for(loc in locations){//for every location
-		navPaths(0, loc, locations);
+		var locs = Object.assign(locations);
+		console.log("locs: " + locs);
+		console.log("starting location is: " + locations[loc]);
+		navPaths(0, locations[loc], locs);
 	}
+
+	console.log("shortest distance is: " + shortest);
 
 }
 
 function navPaths(totDistance, currentLoc, remaining){
-	//first remove currentLoc from list of remaining
-	remaining.pop(currentLoc);
+	//first remove currentLoc from list of remaining;
+	var index = remaining.indexOf(currentLoc);
+	var removed  = Object.assign(remaining[index]);
+	remaining.splice(index, 1);
+	console.log("");
+	console.log("remaining locations: " + remaining);
+	console.log("totDistance: " + totDistance);
+
+	//get all locations that currentLoc could go to (remaining),
+	//and for each one, navPaths again with new remaining and new distance
+
+	if(remaining.length == 0){
+		if(totDistance < shortest){
+			shortest = totDistance;
+		}
+	}
+	else{
+		for(next in remaining){
+			console.log("next location is: " + remaining[next]);
+			//get distance from currentLoc
+			//how to check if both values are in dictionary of distance?
+			var distance = 0;
+			Object.keys(distances).forEach(function(key) {
+			    value = distances[key];
+			    if(value.includes(currentLoc) && value.includes(remaining[next])){
+					console.log("distance between " + currentLoc + " and " + remaining[next] + " is: " + key);
+					distance = parseInt(key);
+				}
+			});
+			navPaths((totDistance + distance), remaining[next], remaining);
+		}
+	}
+
 }
